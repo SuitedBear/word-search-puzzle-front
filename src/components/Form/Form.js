e48dec64c5ae4bd16bdfe15deb67dc6bca67b17b
SuitedBear <SuitedBear@users.noreply.github.com>
2019-07-30 17:32:32 +0200

    form fetch added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,20 +4,65 @@ export default class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       
+      gridWidth: '10',
+      gridHeight: '10',
+      numberOfWords: '10',
+      generatorUrl: 'http://localhost:3001/',
     };
     
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   
+  handleChange(event) {
+    const target = event.target;
+    let { name, value } = target;
+
+    this.setState({
+      [name]: Number(value)
+    });
+  }
+
   handleSubmit(event) {
     event.preventDefault();
+    // conver state to numbers
+    let { gridWidth, gridHeight, numberOfWords} = this.state;
+    const request = `${this.state.generatorUrl}puzzle?width=${gridWidth}&height=${gridHeight}&words=${numberOfWords}`;
+    // const request = this.state.generatorUrl;
+    console.log(request);
+    fetch(request)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(e => console.log(e));
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label></label>
+        <label>
+          Width:
+          <input name="gridWidth" 
+            type="number" min="3" max="30" 
+            value={this.state.gridWidth}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Height:
+          <input name="gridHeight"
+            type="number" min="3" max="30" 
+            value={this.state.gridHeight}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          # of words:
+          <input name="numberOfWords"
+            type="number" min="1" max="30" 
+            value={this.state.numberOfWords}
+            onChange={this.handleChange}
+          />
+        </label>
         <input type="submit" value="Get Puzzle" />
       </form>
     )
