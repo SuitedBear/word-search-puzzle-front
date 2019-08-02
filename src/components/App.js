0e2d23e0fd70b913b7206bee94d004711088c18d
SuitedBear <SuitedBear@users.noreply.github.com>
2019-08-02 18:21:23 +0200

    add grid mapping, state mock

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,70 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      wordList: [],
-      grid: [],
+      wordList: [
+        "kraków",
+        "warszawa",
+        "wrocław",
+        "poznań",
+        "gdańsk",
+        "gdynia",
+        "kielce",
+        "radom",
+        "łódź"
+      ],
+      grid: [
+        ['w', 'r', 'o', 'k', 's', 'n', 'a', 'd'],
+        ['r', 'a', 'k', 'i', 'e', 'l', 'c', 'e'],
+        ['o', 'd', 'r', 'z', 'p', 'o', 'ł', 'g'],
+        ['c', 'o', 'a', 's', 'o', 'r', 'ó', 'd'],
+        ['ł', 'm', 'k', 'i', 'z', 'e', 'd', 'y'],
+        ['a', 'g', 'ó', 'p', 'n', 'a', 'ź', 'n'],
+        ['w', 'e', 'w', 'k', 'a', 't', 'w', 'i'],
+        ['a', 'g', 'd', 'a', 'ń', 's', 'k', 'a'],
+      ],
     }
+  }
+
+  mockGrid() {
+    this.setState({
+      wordList: [
+        "kraków",
+        "warszawa",
+        "wrocław",
+        "poznań",
+        "gdańsk",
+        "gdynia",
+        "kielce",
+        "radom",
+        "łódź"
+      ],
+      grid: [
+        ['w', 'r', 'o', 'k', 's', 'n', 'a', 'd'],
+        ['r', 'a', 'k', 'i', 'e', 'l', 'c', 'e'],
+        ['o', 'd', 'r', 'z', 'p', 'o', 'ł', 'g'],
+        ['c', 'o', 'a', 's', 'o', 'r', 'ó', 'd'],
+        ['ł', 'm', 'k', 'i', 'z', 'e', 'd', 'y'],
+        ['a', 'g', 'ó', 'p', 'n', 'a', 'ź', 'n'],
+        ['w', 'e', 'w', 'k', 'a', 't', 'w', 'i'],
+        ['a', 'g', 'd', 'a', 'ń', 's', 'k', 'a'],
+      ],
+    });
+  }
+
+  handleCellClick(e) {
+    console.log(e.target.value);
   }
 
   render() {
     return (
       <div className="App">
         <h1>Test</h1>
-        <Board />
+        <Board grid={this.state.grid} clickHandler={this.handleCellClick} />
         <Form />
       </div>
     );    
   }
+
 }
 
 export default App;
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Board extends Component {
-  render() {
-    return (
-      <div>
-        plansza
-      </div>
-    )
-  }
+function Board(props) {
+  const lines = props.grid.map((line, index) => {
+    return <BoardLine key={index} line={line} clickHandler={props.clickHandler} />
+  });
+
+  return (
+    // temporary styling
+    <ul style={{ listStyle: 'none' }}>
+      {lines}
+    </ul>
+  )
 }
+
+function BoardLine(props) {
+  const cellLine = props.line.map((cell, index) => {
+    return (
+      <button key={index} onClick={props.clickHandler} value={cell}>{cell}</button>
+    );
+  })
+
+  return (
+    <li>
+      {cellLine}
+    </li>
+  )
+}
+
+
+export default Board;
